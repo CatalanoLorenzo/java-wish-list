7b7facd298e3148a546877c57f5246c746b4c961
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-09-21 12:59:13 +0200

    add bonus for frist slack

--- src/Snack1.java
+++ src/Snack1.java
@@ -11,6 +11,9 @@
 //- ogni elemento salvato in lista non deve avere solo il nome ma anche a chi è destinato (mamma, papà, cugino,  …)
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Snack1 {
@@ -19,18 +22,27 @@ public static void main(String[] args) {
 	ArrayList<String> listaRegali = new ArrayList<String>();
 	
 	Scanner input = new Scanner(System.in);
-	
+	HashSet<String> destinatari = new HashSet<String>()	;
+	Map<String, String> listaRegaliConDestinatari = new HashMap<>();
 	while(true) {
 		System.out.println(" La tua lista regali ha : " + listaRegali.size() + " articoli ");
 		System.out.print("Quale regalo vuoi inserire nella lista ? :");
 		String nuovoRegalo=input.nextLine() ;
-		
 		if(nuovoRegalo.toLowerCase().equals("fine")) {
 			break;
 		}
+		System.out.print("A chi è destinato il regalo? :");
+		String destinatario=input.nextLine() ;
+		if(listaRegaliConDestinatari.containsKey(destinatario)) {
+			listaRegaliConDestinatari.put(destinatario,listaRegaliConDestinatari.get(destinatario)+ "," + nuovoRegalo);
+		}else {
+			listaRegaliConDestinatari.put(destinatario, nuovoRegalo);
+		}
+
+	
 		listaRegali.add(nuovoRegalo);
 	}
-	System.out.println(	listaRegali.toString());
+	System.out.println(	listaRegaliConDestinatari.toString());
 
 	
 }
